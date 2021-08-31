1f536c754dfb61fefcf9e184a70ab92714528eb5
NathanV <nathan.t.vik@gmail.com>
2021-08-30 22:46:00 -0400

    fsdi110 class 2

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import storeContext from '../store/storeContext';
 import ItemInCart from './itemInCart';
+import ItemService from '../services/itemService';
 
 import "./cart.css";
 
 
 class Cart extends Component {
     static contextType = storeContext;
-    state = {  }
+    state = { 
+        couponCode: "",
+        discount: 0.0,
+     }
     render() { 
         return ( 
             <div className="cartPage">
@@ -20,23 +24,63 @@ class Cart extends Component {
                         </div>
                         <div className="cart-total">
                             <h4>Total Price: ${this.getTotal()}</h4>
-                            <button className="btn-vittle">Confirm Order</button>
+                            <input type="text" placeholder="Coupon Code" name ="couponCode" value={this.state.couponCode} onChange={this.handleInputChange}></input>
+                            <button className="btn-vittle" onClick={this.applyCoupon}>Apply Coupon</button>
+                            <br></br>
+                            <br></br>
+                            <button className="btn-vittle" onClick={this.confirmOrder}>Confirm Order</button>
                         </div>
                     </div>
             </div>
          );
     }
 
+applyCoupon = async () => {
+    // validate on server
+    let service = new ItemService();
+    let response = await service.validateCoupon(this.state.couponCode);
+    if (!response) {
+        console.log("Invalid Code!");
+        this.setState({ "discount": 0});
+        return;
+    };
+    
+    console.log(response);
+    this.setState({ "discount": response.discount });
+}
+
 getTotal = () => {
     let cartTotal = 0;
     for( let i = 0; i < this.context.cart.length; i++){
         cartTotal += (this.context.cart[i].price * this.context.cart[i].quantity);
     };
-    return cartTotal.toFixed(2);
+    let discTotal = (cartTotal - (cartTotal * this.state.discount));
+    console.log(discTotal);
+    return discTotal.toFixed(2);
     }
 
+confirmOrder = () => {
+    let order = {
+        "userID" : 123,
+        "discountCode": (this.state.couponCode),
+        "cart": (this.context.cart),
+    }
+    
+    console.log(order);
+    console.log(JSON.stringify(order));
+};
+
+handleInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+};
 
 
 }
  
 export default Cart;
+
+// 
+//  on click of order button
+// console log order
+// userID, discount, products diplayed
+// 
--- src/services/itemService.js
+++ src/services/itemService.js
@@ -3,47 +3,46 @@ import axios from "axios";
 var catalog = [];
 // 9 products minimum
 
-class Product {
-  constructor(_id, title, category, price, image, minimum) {
-    this._id = _id;
-    this.title = title;
-    this.category = category;
-    this.price = price;
-    this.image = image;
-    this.minimum = minimum;
-  }
-}
+// class Product {
+//   constructor(_id, title, category, price, image, minimum) {
+//     this._id = _id;
+//     this.title = title;
+//     this.category = category;
+//     this.price = price;
+//     this.image = image;
+//     this.minimum = minimum;
+//   }
+// }
 
-var strawberry = new Product(
-  1,
-  "Strawberries",
-  "Fruit",
-  1.99,
-  "strawberry.jpg",
-  1
-);
-var apple = new Product(2, "Apples", "Fruit", 1.59, "apple.jpg", 3);
-var milk = new Product(3, "Milk", "Dairy", 1.99, "milk.jpg", 1);
-var bread = new Product(4, "Loaf of Bread", "Bakery", 2.49, "bread.jpg", 2);
-var cheese = new Product(5, "Cheese", "Dairy", 2.99, "cheese.jpg", 1);
-var steak = new Product(6, "Steak", "Meat", 9.99, "steak.jpg", 2);
-var chicken = new Product(7, "Chicken", "Meat", 7.99, "chicken.jpg", 1);
-var chocolate = new Product(8, "Chocolate", "Sweets", 1.39, "chocolate.jpg", 3);
-var coffee = new Product(9, "Coffee", "Drinks", 4.99, "coffee.jpg", 1);
+// var strawberry = new Product(
+//   1,
+//   "Strawberries",
+//   "Fruit",
+//   1.99,
+//   "strawberry.jpg",
+//   1
+// );
+// var apple = new Product(2, "Apples", "Fruit", 1.59, "apple.jpg", 3);
+// var milk = new Product(3, "Milk", "Dairy", 1.99, "milk.jpg", 1);
+// var bread = new Product(4, "Loaf of Bread", "Bakery", 2.49, "bread.jpg", 2);
+// var cheese = new Product(5, "Cheese", "Dairy", 2.99, "cheese.jpg", 1);
+// var steak = new Product(6, "Steak", "Meat", 9.99, "steak.jpg", 2);
+// var chicken = new Product(7, "Chicken", "Meat", 7.99, "chicken.jpg", 1);
+// var chocolate = new Product(8, "Chocolate", "Sweets", 1.39, "chocolate.jpg", 3);
+// var coffee = new Product(9, "Coffee", "Drinks", 4.99, "coffee.jpg", 1);
 
-catalog.push(strawberry);
-catalog.push(apple);
-catalog.push(milk);
-catalog.push(bread);
-catalog.push(cheese);
-catalog.push(steak);
-catalog.push(chicken);
-catalog.push(chocolate);
-catalog.push(coffee);
+// catalog.push(strawberry);
+// catalog.push(apple);
+// catalog.push(milk);
+// catalog.push(bread);
+// catalog.push(cheese);
+// catalog.push(steak);
+// catalog.push(chicken);
+// catalog.push(chocolate);
+// catalog.push(coffee);
 
 class ItemService {
   async getCatalog() {
-    //return catalog;
     //logic to call server
     //retrieve array of products
     let response = await axios.get('http://127.0.0.1:5000/api/catalog');
@@ -54,6 +53,14 @@ class ItemService {
   }
 
   getItemDetails(id) {}
+
+  // create a method to validate coupon code on server
+  // /api/coupons/search/<id>
+  async validateCoupon(code) {
+    let response = await axios.get('http://127.0.0.1:5000/api/coupons/search/' + code);
+    return response.data;
+  }
+
 }
 
 export default ItemService;
